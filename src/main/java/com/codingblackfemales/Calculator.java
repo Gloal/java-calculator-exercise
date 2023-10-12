7b1b189ecedba163776efa13709da721d01f7e68
GiMac <gimac@MacBook-Pro.lan>
2023-10-12 23:15:44 +0100

    Test Classes added

--- src/main/java/com/codingblackfemales/Calculator.java
+++ src/main/java/com/codingblackfemales/Calculator.java
@@ -1,6 +1,11 @@
 package com.codingblackfemales;
 
 public class Calculator {
+
+    public static void main(String[] args) {
+        //How to catch an integer overflow
+        System.out.println(Calculator.multiply(500000000, 1800));
+    }
     public static Integer add(final int firstInput, final int secondInput) {
         return firstInput + secondInput;
     }
@@ -9,11 +14,11 @@ public class Calculator {
         return firstInput - secondInput;
     }
 
-    public static Integer multiply(final int firstInput, final int secondInput) {
+    public static Long multiply(final long firstInput, final long secondInput) {
         return firstInput * secondInput;
     }
 
-    public static Integer divide(final int firstInput, final int secondInput) {
+    public static Double divide(final double firstInput, final double secondInput) {
         return firstInput / secondInput;
     }
 }
--- src/test/java/com/codingblackfemales/CalculatorTests.java
+++ src/test/java/com/codingblackfemales/CalculatorTests.java
@@ -17,10 +17,67 @@ public class CalculatorTests {
     }
 
     @Test
+    @DisplayName("add two big numbers")
+    public void testAdditionBigNumbers() {
+        final Integer sum = Calculator.add(2000000000, 800000000);
+
+        assertEquals(10000000000, sum);
+    }
+
+    @Test
     @DisplayName("subtract two numbers")
     public void testSubtraction() {
         final Integer difference = Calculator.subtract(2, 4);
 
         assertEquals(-2, difference);
     }
+
+    @Test
+    @DisplayName("Multiply two numbers")
+    public void testMultiply(){
+        final Long product = Calculator.multiply(6L,2L);
+
+        assertEquals(12, product);
+    }
+
+    @Test
+    @DisplayName("Multiply by Zero")
+    public void testMultiplyByZero(){
+        final Long product = Calculator.multiply(7,0);
+
+        assertEquals(0, product);
+    }
+
+    @Test
+    @DisplayName("Multiply very big number")
+    public void testMultiplyTooLarge(){
+        final Long product = Calculator.multiply(500000000, 18);
+
+        assertEquals(9000000000L, product);
+    }
+
+    @Test
+    @DisplayName("Divide two integer numbers and return double")
+    public void  testDivision(){
+        final Double quotient = Calculator.divide(55, 100);
+
+        assertEquals(0.55, quotient);
+    }
+
+    @Test
+    @DisplayName("Divide two integers return zero")
+    public void testDivisionByZero(){
+        final Double quotient = Calculator.divide(0, 100);
+
+        assertEquals(0, quotient);
+    }
+
+    @Test
+    @DisplayName("Divide two integers return infinity")
+    public void testDivisionZero(){
+        final Double quotient = Calculator.divide(100, 0);
+
+        assertEquals(Double.POSITIVE_INFINITY, quotient);
+    }
+
 }
